fd600d7929fb692d090f22de9452c79c8bba1f6b
Zayan <alu0101465777@ull.edu.es>
2025-04-04 12:42:43 +0100

    Data Structures and Shared Buffers

--- producer-consumer.cc
+++ producer-consumer.cc
@@ -20,3 +20,21 @@ constexpr int BUFFER_SIZE = 10;
 constexpr int MAX_ITERATIONS = 20;
 constexpr int WRITER_DELAY_MS = 1000;
 constexpr int READER_DELAY_MS = 1500;
+
+// Shared data structures
+std::vector<int> vector_base(N);
+std::queue<int> buffer;
+
+// Synchronization mechanisms
+std::mutex mtx;
+std::mutex cout_mutex;
+std::condition_variable buffer_not_full;
+std::condition_variable buffer_not_empty;
+std::atomic<bool> termination_flag{false};
+
+// Reader history
+std::vector<int> reader1_history;
+std::vector<int> reader2_history;
+std::vector<int> reader3_history;
+
+
